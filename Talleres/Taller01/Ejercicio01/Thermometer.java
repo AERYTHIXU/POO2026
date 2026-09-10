52f7e6b8b481158b11c46e0c547519ae9c42f574
AERYTHIX <321923673+AERYTHIXU@users.noreply.github.com>
2026-09-10 17:54:05 -0500

    Fix declared package

---
+++ Talleres/Taller01/Ejercicio01/Thermometer.java
@@ -0,0 +1,26 @@
+package Talleres.Taller01.Ejercicio01;
+
+public class Thermometer {
+    private double temperature;
+
+    public Thermometer(double initialTemperature) {
+        if (initialTemperature >= -50 && initialTemperature <= 100) {
+            this.temperature = initialTemperature;
+        }
+        else {
+            this.temperature = 0; // Valid default value.
+        }
+    }
+
+    public boolean setTemperature(double newTemperature) {
+        if (newTemperature >= -50 && newTemperature <= 100) {
+            this.temperature = newTemperature;
+            return true;
+        }
+        return false;
+    }
+
+    public double getTemperature() {
+        return this.temperature;
+    }
+}
