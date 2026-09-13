08bdfe06dcdcabefd843aaefd06767d18b1e9fd8
Alejandro Aguirre Torres <321923673+AERYTHIXU@users.noreply.github.com>
2026-09-13 23:02:44 +0000

    Delete Ejercicio05

--- Talleres/Taller01/Ejercicio05/RoomReservation.java
+++
@@ -1,9 +0,0 @@
-package Talleres.Taller01.Ejercicio05;
-
-public class RoomReservation {
-    private String guest;
-    private int roomNumber;
-    private int numberOfNights;
-    private int pricePerNight;
-    
-}
